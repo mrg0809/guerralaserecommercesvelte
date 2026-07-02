11b03e8f8b3e31364a600b4ec77a3b6dca3507aa
mrg0809 <mrg0809@gmail.com>
2026-07-02 11:06:02 -0600

    vercel error

--- src/routes/api/generate-quotation-chat/+server.ts
+++ src/routes/api/generate-quotation-chat/+server.ts
@@ -1,6 +1,6 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private';
 import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { GoogleGenerativeAI } from '@google/generative-ai';
